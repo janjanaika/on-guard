8a13d5b4eb3b32fd1974be406c88f41635b9a44f
lunalovecode <65585638+lunalovecode@users.noreply.github.com>
2024-02-10 13:13:10 +0800

    Add some (mostly) cosmetic changes

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 from googletrans import Translator
 import smtplib
 from email.mime.text import MIMEText
+import re
 
 raw_spam_data = pd.read_csv("Phishing_Email.csv")
 spam_data = raw_spam_data.where((pd.notnull(raw_spam_data)),"")
@@ -30,9 +29,9 @@ model = LogisticRegression()
 model.fit(X_train_features, y_train)
 
 form = st.form(key="my_form")
-user_email = form.text_input("Gmail")
+user_email = form.text_input("Gmail (optional)")
 text = form.text_area("Text to analyze")
-files = form.file_uploader(label="Upload .txt files", type=["txt", "docx", "odt"], accept_multiple_files=True)
+files = form.file_uploader(label="Upload .txt files", type=["txt"], accept_multiple_files=True)
 submit_button = form.form_submit_button(label="Submit")
 
 def flag(text):
@@ -47,7 +46,9 @@ txt = ""
 file_txt = []
 
 if submit_button:
-   if len(text) > 0:
+   if len(text) == 0 or text.isspace():
+      txt = "This text is empty!"
+   else:
       text = translator.translate(text).text
       if flag(text)[0] == 1:
          txt = ":red[Threat detected in the provided text!]"
@@ -60,7 +61,7 @@ if submit_button:
    if len(files) > 0:
       for f in files:
          content = f.getvalue().decode()
-         if len(content) == 0:
+         if len(content) == 0 or content.isspace():
             file_txt.append(f"{f.name} is empty!")
          else:
             content = translator.translate(content).text
@@ -72,17 +73,21 @@ if submit_button:
             else:
                file_txt.append(f"{f.name} is safe!")
 
-   email_text = "\n".join(email_text)
-   msg = MIMEText(email_text)
-   msg["From"] = st.secrets["email"]
-   msg["To"] = user_email
-   msg["Subject"] = "Results from Flagged's text analysis"
-   server = smtplib.SMTP('smtp.gmail.com', 587)
-   server.starttls()
-   server.login(st.secrets["email"], st.secrets["password"])
-   server.sendmail(st.secrets["email"], user_email, msg.as_string())
-   server.quit()
-   st.success("An email containing the results has been sent to your account! (If you don't see it, check your spam folder)")
+   email_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
+   if re.fullmatch(email_pattern, user_email):
+      email_text = "\n".join(email_text)
+      msg = MIMEText(email_text)
+      msg["From"] = st.secrets["email"]
+      msg["To"] = user_email
+      msg["Subject"] = "Results from Flagged's text analysis"
+      server = smtplib.SMTP('smtp.gmail.com', 587)
+      server.starttls()
+      server.login(st.secrets["email"], st.secrets["password"])
+      server.sendmail(st.secrets["email"], user_email, msg.as_string())
+      server.quit()
+      if len(email_text) > 0:
+         st.error("There was a threat in your provided file(s)! More details will be sent through your email. (If you don't see it, check your spam folder.)")
+
 
 st.write(txt)
 for t in file_txt:
